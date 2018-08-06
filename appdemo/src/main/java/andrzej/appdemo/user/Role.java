e23e32e395874d39cc909c865c80c6f547b22874
andrz <andrz@192.168.0.4>
2018-08-06 11:15:33 +0200

    Dodano klasy - encje USer.java oraz Role.java

---
+++ appdemo/src/main/java/andrzej/appdemo/user/Role.java
@@ -0,0 +1,40 @@
+package andrzej.appdemo.user;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "role")
+public class Role {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "role_id")
+	private int id;
+	
+	@Column(name = "role")
+	@NotNull
+	private String role;
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+	
+	
+
+}
---
+++ appdemo/src/main/java/andrzej/appdemo/user/User.java
@@ -0,0 +1,105 @@
+package andrzej.appdemo.user;
+
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "user")
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "user_id")
+	private int id;
+	
+	@Column(name = "email")
+	@NotNull
+	private String email;
+	
+	@Column(name = "password")
+	@NotNull
+	private String password;
+	
+	@Column(name = "name")
+	@NotNull
+	private String name;
+	
+	@Column(name = "last_name")
+	@NotNull
+	private String lastName;
+	
+	@Column(name = "active")
+	@NotNull
+	private int active;
+	
+	@ManyToMany(cascade = CascadeType.ALL)
+	@JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+	private Set<Role> roles;
+	
+	@Transient
+	private String operacja;
+	
+	
+	//gettery i settery
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public int getActive() {
+		return active;
+	}
+	public void setActive(int active) {
+		this.active = active;
+	}
+	public Set<Role> getRoles() {
+		return roles;
+	}
+	public void setRoles(Set<Role> roles) {
+		this.roles = roles;
+	}
+	public String getOperacja() {
+		return operacja;
+	}
+	public void setOperacja(String operacja) {
+		this.operacja = operacja;
+	}
+	
+}
